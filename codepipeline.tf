a37e2cf80f7d072cbea621a1a823e0f7a4b2a481
salvadorm271 <guardianes27@outlook.com>
2023-03-27 19:05:32 -0600

    added pipelines for infra

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,3 +1,5 @@
+// ----------------------------------------------credentials-----------------------------------------------
+
 // i decided to put this here and not inside the module to make this as modular as possible
 
 data "aws_secretsmanager_secret" "git-credentials" {
@@ -8,7 +10,9 @@ data "aws_secretsmanager_secret_version" "git-credentials" {
   secret_id = data.aws_secretsmanager_secret.git-credentials.id
 }
 
-// DONT USE BRANCHES WITH UNDERSCORE
+// DONT USE BRANCHES WITH UNDERSCORE (i use a temp fix that can be seen in the module)
+
+// ------------------------------------------app repo pipelines-----------------------------------------------
 
 // change name of module
 module aws_codepipeline_feature {
@@ -89,5 +93,56 @@ module aws_codepipeline_main {
 
 }
 
+// ------------------------------------------infra repo pipelines----------------------------------------------
 
+module aws_codepipeline_feature_infra {
+  source = "./modules/aws_codepipeline"
+  
+  codebuild_project_name = "codebuild-aws-pipeline-infra"
+
+  github_token = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["github_token"]
+
+  bucket_name = "test-for-codepipeline-4736478364-infra"
+
+  pipeline_name = "mern-app-pipeline-infra"
+
+  frontend_repo = "" // not needed for this, but only use to create env so can be blank
+
+  git_email = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_email"]
+
+  git_user = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_user"]
+
+  git_repo = "eks_terraform" // change repo if needed
+
+  git_branch = "feature/new_feature" // change branch
+
+  buildspec_file = "buildspec.yml" // change codebuild file name
+
+}
+
+
+module aws_codepipeline_develop_infra {
+  source = "./modules/aws_codepipeline"
+  
+  codebuild_project_name = "codebuild-aws-pipeline-infra"
+
+  github_token = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["github_token"]
+
+  bucket_name = "test-for-codepipeline-4736478364-infra"
+
+  pipeline_name = "mern-app-pipeline-infra"
+
+  frontend_repo = "" // not needed for this, but only use to create env so can be blank
+
+  git_email = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_email"]
+
+  git_user = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_user"]
+
+  git_repo = "eks_terraform" // change repo if needed
+
+  git_branch = "develop" // change branch
+
+  buildspec_file = "buildspec_dev.yml" // change codebuild file name
+
+}
 

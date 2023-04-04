4a6fc8ffb5976c52e48302199017886bcd16c533
salvadorm271 <guardianes27@outlook.com>
2023-04-04 12:58:24 -0600

    change workflow on infra repo

--- codepipeline.tf
+++ codepipeline.tf
@@ -45,7 +45,7 @@ module aws_codepipeline_feature {
 }
 
 // change name of module
-module aws_codepipeline_develop {
+module aws_codepipeline_main {
   source = "./modules/aws_codepipeline"
   
   codebuild_project_name = "codebuild-aws-pipeline"
@@ -68,9 +68,38 @@ module aws_codepipeline_develop {
 
   git_repo = "testmernapp"
 
-  git_branch = "develop" // change branch
+  git_branch = "main" // change branch
 
   buildspec_file = "buildspec-dev.yml" // change codebuild file name
+
+}
+
+module aws_codepipeline_stg {
+  source = "./modules/aws_codepipeline"
+  
+  codebuild_project_name = "codebuild-aws-pipeline"
+
+  github_token = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["github_token"]
+
+  bucket_name = "test-for-codepipeline-4736478364"
+
+  pipeline_name = "mern-app-pipeline"
+
+  frontend_repo = "153042419275.dkr.ecr.us-east-1.amazonaws.com/eks_mern_frontend_staging" // change repo
+
+  git_email = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_email"]
+
+  git_user = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_user"]
+
+  docker_user = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["docker_user"]
+
+  docker_password = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["docker_password"]
+
+  git_repo = "testmernapp"
+
+  git_branch = "staging" // change branch
+
+  buildspec_file = "buildspec-stg.yml" // change codebuild file name
 
 }
 
@@ -107,7 +136,7 @@ module aws_codepipeline_main {
 
 // ------------------------------------------infra repo pipelines----------------------------------------------
 
-module aws_codepipeline_feature_infra {
+module aws_codepipeline_main_infra {
   source = "./modules/aws_codepipeline"
   
   codebuild_project_name = "codebuild-aws-pipeline-infra"
@@ -118,7 +147,7 @@ module aws_codepipeline_feature_infra {
 
   pipeline_name = "mern-pipeline-infra"
 
-  frontend_repo = "" // not needed for this, but only use to create env so can be blank
+  frontend_repo = "" // not needed for this, only use to create env so can be blank (creates blank env)
 
   git_email = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_email"]
 
@@ -126,35 +155,9 @@ module aws_codepipeline_feature_infra {
 
   git_repo = "eks_terraform" // change repo if needed
 
-  git_branch = "feature/new_feature" // change branch
+  git_branch = "main" // change branch
 
   buildspec_file = "buildspec.yml" // change codebuild file name
-
-}
-
-
-module aws_codepipeline_develop_infra {
-  source = "./modules/aws_codepipeline"
-  
-  codebuild_project_name = "codebuild-aws-pipeline-infra"
-
-  github_token = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["github_token"]
-
-  bucket_name = "test-for-codepipeline-4736478364-infra"
-
-  pipeline_name = "mern-pipeline-infra"
-
-  frontend_repo = "" // not needed for this, but only use to create env so can be blank
-
-  git_email = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_email"]
-
-  git_user = jsondecode(data.aws_secretsmanager_secret_version.git-credentials.secret_string)["git_user"]
-
-  git_repo = "eks_terraform" // change repo if needed
-
-  git_branch = "develop" // change branch
-
-  buildspec_file = "buildspec_dev.yml" // change codebuild file name
 
 }
 
